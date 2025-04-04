153934412d5657f02425ed3e7ba4cb573ba82bd9
Seu Nome <seu.email@exemplo.com>
2025-04-04 09:25:50 -0300

    Corrigindo configuração do Vercel e adicionando tratamento de erros

--- server.js
+++ server.js
@@ -9,17 +9,47 @@ app.set("views", path.join(__dirname, "views"));
 // Arquivos estáticos
 app.use("/static", express.static(path.join(__dirname, "static")));
 
+// Middleware para processar dados do formulário
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Rotas
 app.get("/", (req, res) => {
-  res.render("index");
+  try {
+    res.render("index");
+  } catch (error) {
+    console.error("Erro ao renderizar index:", error);
+    res.status(500).send("Erro interno do servidor");
+  }
 });
 
 app.get("/imoveis", (req, res) => {
-  res.render("imoveis");
+  try {
+    res.render("imoveis");
+  } catch (error) {
+    console.error("Erro ao renderizar imoveis:", error);
+    res.status(500).send("Erro interno do servidor");
+  }
 });
 
 app.get("/veiculos", (req, res) => {
-  res.render("veiculos");
+  try {
+    res.render("veiculos");
+  } catch (error) {
+    console.error("Erro ao renderizar veiculos:", error);
+    res.status(500).send("Erro interno do servidor");
+  }
+});
+
+// Handler para erros 404
+app.use((req, res) => {
+  res.status(404).render("404");
+});
+
+// Handler para erros gerais
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Algo deu errado!");
 });
 
 // Vercel requer que exportemos o app
